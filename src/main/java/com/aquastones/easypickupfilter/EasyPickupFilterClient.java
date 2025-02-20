dec473641ee30c176b877f610f1fc9b42e53ebd7
aquastones <aquastones@icloud.com>
2025-02-20 18:27:34 +0100

    Initial commit

---
+++ src/main/java/com/aquastones/easypickupfilter/EasyPickupFilterClient.java
@@ -0,0 +1,10 @@
+package com.aquastones.easypickupfilter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EasyPickupFilterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/aquastones/easypickupfilter/EasyPickupFilterDataGenerator.java
@@ -0,0 +1,11 @@
+package com.aquastones.easypickupfilter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EasyPickupFilterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
